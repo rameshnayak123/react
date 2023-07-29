31981443b6f4d06e9888439fc84070c6b6fbb7a4
Ramesh Nayak <drameshnayak0712@gmail.com>
2023-07-29 06:37:23 +0000

    Everything deleted

--- example/src/app/employee/employeeClass.ts
+++
@@ -1,9 +0,0 @@
-export class Employee{
-
-    public constructor(public id:number, public ename:string, public salary:number)
-    {
-        this.id=id;
-        this.ename=ename;
-        this.salary=salary;
-    }
-}
--- example/src/app/employee/employeeData.ts
+++
@@ -1,9 +0,0 @@
-import { Employee }  from './employeeClass';
-
-
-export var empdata:Employee []  = [
-
-    new Employee(1,"Ramesh",2000),
-    new Employee(2,"Suresh",3000),
-    new Employee(3,"Bhanu",1234)
-]
